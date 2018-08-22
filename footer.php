9d04f1b870a0bc03a04e3637df7c506b4f084038
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2018-08-22 13:03:20 +0530

    footer part of the website

---
+++ footer.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<div style="background-color: #2c2d2d; color: white;">
+	  		<div class="row col-md-12 col-sm-12 col-12" style="padding-top: 20px;">
+				<div class="col-md-3 col-sm-3 col-12" align="center">
+					<div class="row col-md-8 col-sm-12 col-12 ">
+						<div class="col-md-3 col-sm-6 col-3">
+							<a href="https://www.facebook.com/PizzaHut/?ref=br_rs&brand_redir=6053772414">
+								<img src="images/facebook.png" style="width: 35px;  ">
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-3">
+							<a href="https://twitter.com">
+								<img src="images/Twitter-icon.png" style="width: 36px;">
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-3">
+							<a href="https://www.instagram.com/">
+								<img src="images/instagram.png" style="width: 35px;">
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-3">
+							<a href="https://www.pinterest.com/">
+								<img src="images/pinterest.png" style="width: 35px;">
+							</a>
+						</div>
+					</div>
+						
+					<div style=" text-align: center;">
+						<p>Follow Us</p>
+					</div>
+						
+				</div>
+				<div class="col-md-5 col-sm-5 col-12">
+					<form action="" method="">
+						<div class="row col-md-12 col-sm-12 col-12">
+							<div class="col-md-10 col-sm-10 col-10">
+								<input class="form-control" type="search" placeholder="Type your e-mail here..." aria-label="earch">
+							</div>
+							<div class="col-md-2 col-sm-2 col-2">
+								<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Submit</button>
+							</div>
+						</div>
+					</form>
+					<p style="font-size: 12px;text-align: center;padding-top: 5px;"> To get notifications about our new Deals, Submit your e-mail. We'll never share your e-mail with third party.</p>
+				</div>
+
+				<div class="col-md-4 col-sm-4 col-12" align="center">
+						
+					<div class="row col-md-12 col-sm-12 col-12">
+						<div class="col-md-7 col-sm-12 col-6">
+							<a href="https://play.google.com/store/apps/details?id=lk.gamma.pizzakraft.PizzaHut">
+								<img src="images/aplaystore.png" style="width: 140px;">
+							</a>
+						</div>
+						<div class="col-md-5 col-sm-12 col-6">
+							<a href="https://itunes.apple.com/us/app/pizza-hut-sri-lanka/id1040058416?mt=8">
+								<img src="images/appstore.png" style="width: 140px;">
+							</a>
+						</div>
+					</div>
+					<div style="text-align:  center;">
+						<p>Experience our mobile Apps</p>
+					</div>
+				</div>
+			</div>
+			<hr>
+	  		<p class="para">
+	  			<a href="">Privacy Policy </a>
+	  			|
+	  			<a href=""> Terms & Conditions </a>
+	  		</p>
+	  		<hr>
+	  		<p class="para">
+	  			Contact Us : +94 112 729729/  +94 114 729729/  +94 117 729729<br>
+	  			Official website : http://www.pizzahut.lk<br>
+	  			The Pizza Hut name, logos, and related marks are trademarks of Pizza Hut, Inc.<br>
+				</p>
+				
+	  		<div style="text-align: center; background-color: #474849; padding: 10px; font-size: 13px;">
+	  			All Rights Reserved. Copyright &copy; PizzaHut.lk
+	  		</div>
+
+	  	</div>
+	</body> 
+</html>
+	
