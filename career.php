efb366b30327ce0e290d8af826e2380f18d4031f
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2018-08-23 19:44:02 +0530

    career page

---
+++ career.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="style-index.css">
+  <script src="jquery/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.js"></script>
+    <title>PizzaHut.lk/Career</title>
+</head>
+<body style="background-image:url(images/wb4.jpg);">
+  
+    <?php include 'header.php'; ?>
+  
+    <div class="container" style="width: 700px; padding-bottom: 100px;">
+      <p><h1 style="text-align: center; font-weight: bold; color: #e58100">Career</h1></p>
+      <hr>
+      <p style="text-align: center; font-size: 13px;color: #e58100; padding-bottom: 50px;">Find your Career. Join with us.</p>
+      <div class="career col-md-12 col-sm-12 col-6">
+        Restaurant Supervisor (Full-Time)   
+        <a href="#" class="btn btn-success btn-sm">Apply</a>
+      </div>
+      <div class="career col-md-12 col-sm-12 col-6">
+        Restaurant Supervisor (Part-Time)   
+        <a href="#" class="btn btn-success btn-sm">Apply</a>
+      </div>
+      <div class="career col-md-12 col-sm-12 col-6">
+        Pizza Diliverer   
+        <a href="#" class="btn btn-success btn-sm">Apply</a>
+      </div>
+      
+    </div>
+
+    <?php include 'footer.php'; ?>
+      
+</body>
+</html>
