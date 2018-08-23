19e402e2b9208ce559638f972443e20316fcd1e2
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2018-08-23 19:41:21 +0530

    about us page

---
+++ about_us.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="style-index.css">
+  <script src="jquery/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.js"></script>
+    <title>PizzaHut.lk/About_Us</title>
+</head>
+<body style="background-image:url(images/wb4.jpg);">
+  
+    <?php include 'header.php'; ?>
+
+    <div class="container" style="color: #b7b5b3; width: 70%; padding-bottom: 40px;">
+      <p><h1 style="text-align: center; font-weight: bold; color: #e58100">About Us</h1></p>
+      <hr>
+      <p>
+        Pizza Hut, a subsidiary of Yum! Brands, is the world's largest pizza company and home of the Pan Pizza. Pizza Hut began 55 years ago in Wichita, Kansas, and today is an iconic global brand that delivers more pizza, pasta and wings than any other restaurant in the world. 
+      </p>
+      <p>
+        Since its inception in 1993, Pizza Hut has fast become a household name across Sri Lanka. With its first restaurant at Union Place, Colombo 2, Pizza Hut became the first international restaurant chain to begin operations in Sri Lanka. Pizza Hut is a brand name belonging to global restaurant chain Yum Brands Incorporated which is based in USA. It is the world's largest restaurant company which also owns KFC & Taco Bell. 
+      </p>
+      <p>
+        In 1999, Pizza Hut ventured into the home delivery service setting yet another innovative milestone in Sri Lanka. With the launch of its islandwide delivery hotline 011 2 729729, Pizza Hut delivery rose swiftly to provide 50% of the restaurants net sales. 
+      </p>
+      <p>
+        To date, Pizza Hut Sri Lanka employs over 1000 staff members with diverse backgrounds. As an equal opportunity employer, Pizza Hut utilises 1% of its annual income towards training and welfare. The Company strongly believes in creating a results oriented fun working environment. 
+      </p>
+      <p>
+        Over the past 20 years, Pizza Hut has grown from strength to strength. At Pizza Hut we believe in delighting the senses of each and every customer. This is why all dine-in outlets offer a unique family friendly dine-in experience whilst effectively capturing the gastronomic palate of the Sri Lankan consumer with innovative additions to its menu. Currently Pizza Hut serves a wide range of pizza toppings with an exciting selection of special crusts as well as appetizers, pastas, rice, desserts and beverages.
+      </p>
+    </div>
+
+    <?php include 'footer.php'; ?>
+      
+</body>
+</html>
