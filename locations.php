514655a4e10002e94ce65185fb36347bb43340df
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2018-08-23 19:54:57 +0530

    pizzahut centers' locations

---
+++ locations.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="style-index.css">
+  <script src="jquery/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.js"></script>
+    <title>PizzaHut.lk/Locations</title>
+</head>
+<body style="background-image:url(images/wb4.jpg);">
+  
+  <?php include 'header.php'; ?>
+
+    <div>
+      <iframe src="https://www.google.lk/maps" width="700" height="400"></iframe> 
+    </div>
+
+    <?php include 'footer.php'; ?>
+      
+</body>
+</html>
